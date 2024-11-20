dfd6fb51e86fd3b1525a6915800691e5af2e50ce
sraghbav <51275296+sraghbav@users.noreply.github.com>
2024-11-20 10:45:19 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -50,6 +50,33 @@ def naive_bayes(full_table, evidence_row, target_column):
   #do same for P(target=1|...)
   pos = cond_probs_product(full_table, evidence_row, target_column, 1) * prior_prob(full_table, target_column, 1)
 
+def metrics(zipped_list):
+  assert isinstance(zipped_list, list)
+  assert all([isinstance(v, list) for v in zipped_list])
+  assert all([len(v)==2 for v in zipped_list])
+  assert all([isinstance(a,(int,float)) and isinstance(b,(int,float)) for a,b in zipped_list]), f'zipped_list contains a non-int or non-float'
+  assert all([float(a) in [0.0,1.0] and float(b) in [0.0,1.0] for a,b in zipped_list]), f'zipped_list contains a non-binary value'
+
+  #first compute the sum of all 4 cases. See code above
+
+  tp = sum([1 if pair==[1,1] else 0 for pair in zipped_list])
+  tn = sum([1 if pair==[0,0] else 0 for pair in zipped_list])
+  fp = sum([1 if pair==[1,0] else 0 for pair in zipped_list])
+  fn = sum([1 if pair==[0,1] else 0 for pair in zipped_list])
+
+  #now can compute precicision, recall, f1, accuracy. Watch for divide by 0.
+
+  accuracy = (tp+tn)/(tp+tn+fp+fn) if (tp+tn+fp+fn) != 0 else 0
+  precision = tp/(tp+fp) if (tp+fp) != 0 else 0
+  recall = tp/(tp+fn) if (tp+fn) != 0 else 0
+  f1 = 2*precision*recall/(precision+recall) if (precision+recall) != 0 else 0
+
+  #now build dictionary with the 4 measures - round values to 2 places
+
+  metrics_dict = {'Precision': round(precision,2), 'Recall': round(recall,2), 'F1': round(f1,2), 'Accuracy': round(accuracy,2)}
+
+  #finally, return the dictionary
+  return metrics_dict
   #Use compute_probs to get 2 probabilities
   neg, pos = compute_probs(neg, pos)
   #return your 2 results in a list
